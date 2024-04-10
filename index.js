a7af88a442fcb66e51644240be550f5b1996a6fb
Alisson <alisson_.ferro@hotmail.com>
2024-04-10 16:56:44 -0300

    feat: create socket.io

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
- 
+const { Server } = require('socket.io');
+
 app.use(cors({
     origin: '*'
 }));
@@ -12,5 +13,18 @@ require('./startup/routes')(app);
 const port = 8080;
  
 const server = app.listen(port, () => console.log(`Listening on port ${port}`));
- 
-module.exports = server;
+const socketio = new Server(server, {
+    cors: {
+        origin: '*'
+    }
+});
+socketio.on('connection', (socket) => {
+    console.log('teste');
+    socket.on('disconnect', () => {
+        console.log('usuario desconectado')
+    });
+    socket.on('message', (message) => {
+        socket.broadcast.emit('message', message);
+    })
+})
+module.exports = socketio;

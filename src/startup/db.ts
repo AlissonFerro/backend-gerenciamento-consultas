d423e3afc84a3f16bd554e67a8c61c1139105f36
Ferro Alisson (CtP/ETS) <fel9ct@bosch.com>
2024-10-24 11:58:18 -0300

    update db.ts

--- src/startup/db.ts
+++ src/startup/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 import config from 'config';
 
 export default function(){
-    const db = config.get('db');
+    const db = process.env.DB || config.get('db');
     mongoose.connect(String(db))
         .then(() => console.log(`connected to ${db}`))
 }
